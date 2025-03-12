f99fc54b74ee053f44a057e094ed7144a97962df
Ayush Rawat <146158503+ayushrawat001@users.noreply.github.com>
2025-03-12 21:59:48 +0530

    Update vite.config.js

--- 07_reactRouter/vite.config.js
+++ 07_reactRouter/vite.config.js
@@ -1,34 +1,14 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-  
-//   plugins: [
-//     tailwindcss(),
-//     react()],
-// })
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [tailwindcss(), react()],
-//   server: {
-//     allowedHosts: 58mdwf-5173.csb.app, // Allow all hosts
-//     host: true, // Ensures proper host binding
-//     port: 5173, // You can set a custom port if needed
-//   }
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  
+  plugins: [
+    tailwindcss(),
+    react()],
 })
+
+
 
