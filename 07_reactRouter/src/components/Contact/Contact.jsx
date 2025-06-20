641bcbf6335304a4771518d63bedf0dadc3c0e00
Ayush Rawat <146158503+ayushrawat001@users.noreply.github.com>
2025-06-20 06:26:24 +0530

    Update Contact.jsx

--- 07_reactRouter/src/components/Contact/Contact.jsx
+++ 07_reactRouter/src/components/Contact/Contact.jsx
@@ -38,7 +38,7 @@ export default function Contact() {
                                     />
                                 </svg>
                                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                                    Acme Inc, Street, State, Postal Code
+                                    Jaipur, Rajasthan
                                 </div>
                             </div>
 
@@ -60,7 +60,7 @@ export default function Contact() {
                                     />
                                 </svg>
                                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                                    +44 1234567890
+                                    +91 9351210771
                                 </div>
                             </div>
 
@@ -82,7 +82,7 @@ export default function Contact() {
                                     />
                                 </svg>
                                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                                    info@acme.org
+                                    ayushrawat4659@gmail.com
                                 </div>
                             </div>
                         </div>
